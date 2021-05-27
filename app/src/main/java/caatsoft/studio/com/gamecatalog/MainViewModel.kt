07a2e312e5d19376a8bbad3a0f9297c8e94041e9
Darlei Silva <darlei.p.d.silva@gmail.com>
2021-05-26 23:46:24 -0300

    doing feature network

--- app/src/main/java/caatsoft/studio/com/gamecatalog/MainViewModel.kt
+++ app/src/main/java/caatsoft/studio/com/gamecatalog/MainViewModel.kt
@@ -1,25 +1,3 @@
-/*
- * Copyright (c) 2019 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package caatsoft.studio.com.gamecatalog
 
 import androidx.lifecycle.ViewModel
@@ -31,6 +9,9 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.koin.core.KoinComponent
 import org.koin.core.inject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 interface MainView {
   fun getFilteredGames(GameResponse: GameResponse2)
@@ -41,10 +22,22 @@ class MainViewModel : ViewModel(), KoinComponent {
   var view: MainView? = null
 
   fun getFilteredGames() {
+    var gameResponse: GameResponse2? = null
+
     viewModelScope.launch(Dispatchers.IO) {
       val filteredGames = gameAPIInterface.getFilteredGames("json", "name,deck,platforms,original_release_date,image")
+      filteredGames.enqueue(object : Callback<GameResponse2> {
+        override fun onResponse(call: Call<GameResponse2>, response: Response<GameResponse2>) {
+          response.let {
+            gameResponse = it.body()!!
+
+          }
+        }
+        override fun onFailure(call: Call<GameResponse2>, t: Throwable) {
+        }
+      })
       withContext(Dispatchers.Main) {
-        view?.getFilteredGames(filteredGames)
+        gameResponse?.let { view?.getFilteredGames(it) }
       }
     }
   }
--- app/src/main/java/caatsoft/studio/com/gamecatalog/di/networkModule.kt
+++ app/src/main/java/caatsoft/studio/com/gamecatalog/di/networkModule.kt
@@ -9,6 +9,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 
@@ -43,6 +44,14 @@ val networkModule = module {
             .client(get())
             .build()
     }
+
+    /*single {
+        Retrofit.Builder().baseUrl(get<String>(named("base_url")))
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+    }*/
+
 
     single {
         get<Retrofit>().create(GameAPIInterface::class.java)
