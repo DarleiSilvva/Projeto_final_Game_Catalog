88842f04c5a14bd25bef36d40e06a566db9d6f55
Darlei Silva <darlei.p.d.silva@gmail.com>
2021-05-28 15:46:26 -0300

    bug fix listgame in viewmodel

--- app/src/main/java/caatsoft/studio/com/gamecatalog/MainActivity.kt
+++ app/src/main/java/caatsoft/studio/com/gamecatalog/MainActivity.kt
@@ -1,18 +1,13 @@
 package caatsoft.studio.com.gamecatalog
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModel
 import caatsoft.studio.com.gamecatalog.databinding.ActivityMainBinding
-import caatsoft.studio.com.gamecatalog.network.GameResponse
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import org.koin.core.KoinComponent
 
 
-class MainActivity : AppCompatActivity(), KoinComponent, MainView {
+class MainActivity : AppCompatActivity() {
     private val mainViewModel: MainViewModel by viewModel()
-
     private lateinit var activityMainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,16 +15,10 @@ class MainActivity : AppCompatActivity(), KoinComponent, MainView {
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
 
-        //mainViewModel.view = this
-        //mainViewModel.getFilteredGames()
-
-        mainViewModel.games.observe(this, { gamesList->
+        mainViewModel.games.observe(this, { gamesList ->
             activityMainBinding.textView.text = gamesList[0].deck
         })
-    }
-
-    override fun getFilteredGames(gameResponse: GameResponse) {
-        //activityMainBinding.textView.text = gameResponse[0].deck
+        mainViewModel.getFilteredGames()
     }
 }
 
